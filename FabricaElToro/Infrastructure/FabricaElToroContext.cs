860174d56a9056a078b8152c4c50b0d791f997cf
minikirita2.2@gmail.com <minikirita2.2@gmail.com>
2021-10-29 20:45:54 -0500

    db

---
+++ FabricaElToro/Infrastructure/FabricaElToroContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FabricaElToro.Infrastructure
+{
+    public class FabricaElToroContext : DbContext 
+    {
+        public FabricaElToroContext(DbContextOptions<FabricaElToroContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
